bca0b1886305ed66c35d83409d6475e3079634d1
tamerlan <tronsm2k18@mail.ru>
2023-04-13 02:17:58 +0600

    h_w_39 files

---
+++ h_w_39.py
@@ -0,0 +1,12 @@
+import json
+
+data = {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": False
+}
+
+for key, value in data.items():
+    with open(f'{key}.json', 'w') as f:
+        json.dump({key: value}, f)
